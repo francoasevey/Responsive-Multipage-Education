4904518fb0a98a98095750f9dd85ea9dd219d8bb
francoromeo <francoromeo5012@gmail.com>
2023-09-27 12:33:52 -0300

    faqs

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
 
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = 'uil uil-minus'
+        } else {
+            icon.className = 'uil uil-plus'
+        }
+    })
+})
+
 
 
 
